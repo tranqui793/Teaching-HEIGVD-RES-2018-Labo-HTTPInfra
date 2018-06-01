324d01cf1046b5d02d247f7d9eb1a1c790cd24cb
Adam <adam.zouari@heig-vd.ch>
2018-06-01 10:37:27 +0200

    change the content returned

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -1,35 +1,50 @@
-var Chance =require('chance');
-var express=require('express'); 
-var chance =new Chance(); 
-var app=express(); 
-app.get('/',function(req,res){ res.send(generateStudents());});
-app.listen(3000,function(){console.log('Accepting HTTP requests on port 3000.');});
-function generateStudents(){
-	var numberOfStudents =chance.integer({
-		min:0,
-		max:10
-	});
-console.log(numberOfStudents);
-var students=[];
-for(var i=0;i<numberOfStudents;i++){
-	var gender =chance.gender();
-	var birthYear =chance.year({
-		min: 1986,
-		max:1996
-	});
-students.push({
-	firstName:chance.first({
-	gender:gender
-}),
-lastName:chance.last(),
-gender:gender,
-birthday:chance.birthday({
-year:birthYear
-})
+var Chance = require('chance');
+var chance = new Chance();
+
+var express = require('express');
+var app = express();
+
+// pour afficher de maniere jolie dans le navigateur web
+app.set('json spaces', 40);
+
+// renvoi une liste d'etudiants quand on fait une requete HTTP vers la racine
+app.get('/', function (req, res) {
+	
+	res.json(generateLocations());
 });
-};
-console.log(students);
-return students;
 
+
+app.listen(3000, function () {
+	console.log('Accepting HTTP requests on port 3000.');
+});
+
+function generateLocations() {
+
+	var numberOfLocations = chance.integer({
+		min: 0,
+		max: 10
+	});
+
+	console.log(numberOfLocations);
+	
+	var locations = [];
+	for (var i = 0; i < numberOfLocations; i++) {
+
+		var address = chance.address();
+		var city = chance.city();
+		var country = chance.country({
+			full:true
+		});
+
+		locations.push({
+			address: address,
+			city:city,
+			country:country
+		});
+	};
+
+	// pour afficher de maniere jolie dans les log du serveur
+	console.log(JSON.stringify(locations,null,'\t'));
+
+	return locations;
 }
-
