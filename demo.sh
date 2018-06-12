9aa7c4d107d83c585244c6162cb6e3236e55db9a
Adam <adam.zouari@heig-vd.ch>
2018-06-12 13:03:44 +0200

    update script

---
+++ demo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Script to automatize the lab HTTP Demo
+
+echo "Killing old containers ..."
+docker kill $(docker ps -q) 2>/dev/null 
+docker rm $(docker ps -a -q) 2>/dev/null
+
+echo -e "\nRunning $1 server containers including the 2 principals..."
+docker run -d -it --name apache_php res/apache_php 
+docker run -d -it --name express_locations res/express_dynamic 
+
+for i in `seq 2 $1`
+do
+	docker run -d -it res/apache_php
+	docker run -d -it res/express_dynamic
+done
+
+staticIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' apache_php)
+dynamicIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' express_locations)
+
+echo -e "\nPrincipal static server running on $staticIP \nPrincipal dynamic server running on $dynamicIP"
+
+echo -e "\nReverse proxy running ..."
+docker run -d -p 8080:80 -e STATIC_APP=$staticIP -e DYNAMIC_APP=$dynamicIP:3000 --name apache_rp res/apache_rp
+
